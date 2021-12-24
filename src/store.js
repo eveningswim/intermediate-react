04db0bd763f3c46b85ed397988681d0fdc56d749
Dylan <heyitsdylan@gmail.com>
2021-12-23 19:59:20 -0800

    Redux: action creators

--- src/store.js
+++ src/store.js
@@ -1,8 +1,8 @@
 import { createStore } from "redux";
-import reducer from "/.reducer";
+import reducer from "./reducer";
 
 const store = createStore(
-  store,
+  reducer,
   typeof window === "object" &&
     typeof window.__REDUX_DEVTOOLS_EXTENSION__ !== "undefined"
     ? window.__REDUX_DEVTOOLS_EXTENSION__()
